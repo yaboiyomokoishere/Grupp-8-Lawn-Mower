2dcd7d0b990f2f9f529af6735d7561969eee4a18
Dispatch7478 <nahtse-2@student.ltu.se>
2024-12-16 16:45:03 +0100

    dfs

--- web-app/src/views/LoginView.vue
+++ web-app/src/views/LoginView.vue
@@ -1,5 +1,37 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
+
+import router from '@/router';
+import {reactive} from 'vue';
+import axios from 'axios';
+import {useToast} from "vue-toastification";
+
+
+const credentials = reactive({
+  userEmail: '',
+  password: ''
+})
+
+const toast = useToast();
+
+const handleSubmit = async () => {
+  const newCustomer = {
+  email: credentials.userEmail,
+  password: credentials.password
+  };
+  try {
+    const response = await axios.post('http://localhost:3001/api/user/login', newCustomer);
+    console.log(response.data); // Log the response data obtained from the backend
+    toast.success('Customer created successfully');
+    
+    // Save the 
+
+    //router.push('../login'); // Redirect to the login page after sucessful signup
+  } catch (error) {
+    console.log('Error creating customer:', error);
+    toast.error(error.message);
+  }
+}
 </script>
 
 

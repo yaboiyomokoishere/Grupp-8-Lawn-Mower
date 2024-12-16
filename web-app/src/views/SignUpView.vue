641a8a00fb5a8166acabd336b00205cfffa1a941
Dispatch7478 <nahtse-2@student.ltu.se>
2024-12-16 17:23:21 +0100

    pls dont explode

--- web-app/src/views/SignUpView.vue
+++ web-app/src/views/SignUpView.vue
@@ -48,7 +48,7 @@ const handleSubmit = async () => {
     console.log(response.data); // Log the response data obtained from the backend
     toast.success('Customer created successfully');
     
-    //router.push('../login'); // Redirect to the login page after sucessful signup
+    router.push('../login'); // Redirect to the login page after sucessful signup
   } catch (error) {
     console.log('Error creating customer:', error);
     toast.error(error.message);
